0e25bd2518ef62eebe2c32dc199c57110c7be6a4
Maja Francetic <francetic.maja@gmail.com>
2020-12-03 10:37:31 +0100

    Set the initial properties of the LoadingButton; change text in relation to the ButtonState; draw text

--- starter/app/src/main/java/com/udacity/LoadingButton.kt
+++ starter/app/src/main/java/com/udacity/LoadingButton.kt
@@ -3,44 +3,70 @@ package com.udacity
 import android.animation.ValueAnimator
 import android.content.Context
 import android.graphics.Canvas
+import android.graphics.Color
+import android.graphics.Paint
+import android.graphics.Typeface
 import android.util.AttributeSet
 import android.view.View
+import androidx.core.content.withStyledAttributes
 import kotlin.properties.Delegates
 
 class LoadingButton @JvmOverloads constructor(
-    context: Context, attrs: AttributeSet? = null, defStyleAttr: Int = 0
+        context: Context, attrs: AttributeSet? = null, defStyleAttr: Int = 0
 ) : View(context, attrs, defStyleAttr) {
     private var widthSize = 0
     private var heightSize = 0
 
     private val valueAnimator = ValueAnimator()
 
-    private var buttonState: ButtonState by Delegates.observable<ButtonState>(ButtonState.Completed) { p, old, new ->
+    private var buttonClickedText = 0
+    private var buttonLoadingText = 0
+    private var buttonCompleteText = 0
 
+    private var buttonState: ButtonState by Delegates.observable(ButtonState.Completed) { property, old, new ->
+        buttonState = new
     }
 
+    private val paint = Paint(Paint.ANTI_ALIAS_FLAG).apply {
+        style = Paint.Style.FILL
+        textAlign = Paint.Align.CENTER
+        setBackgroundColor(context.resources.getColor(R.color.colorPrimary))
+        textSize = 55.0f
+        color = Color.WHITE
+        typeface = Typeface.create("", Typeface.NORMAL)
+    }
 
     init {
-
+        isClickable = true
+        context.withStyledAttributes(attrs, R.styleable.LoadingButton) {
+            buttonClickedText = getInt(R.styleable.LoadingButton_buttonClickedText, 0)
+            buttonLoadingText = getInt(R.styleable.LoadingButton_buttonLoadingText, 0)
+            buttonCompleteText = getInt(R.styleable.LoadingButton_buttonCompletedText, 0)
+        }
     }
 
-
-    override fun onDraw(canvas: Canvas?) {
+    override fun onDraw(canvas: Canvas) {
         super.onDraw(canvas)
 
+        val buttonText = when (buttonState) {
+            ButtonState.Clicked -> context.getString(R.string.download)
+            ButtonState.Loading -> context.getString(R.string.button_loading)
+            ButtonState.Completed -> context.getString(R.string.download)
+        }
+
+        canvas.drawText(buttonText, widthSize / 2f, heightSize / 1.7f, paint)
     }
 
     override fun onMeasure(widthMeasureSpec: Int, heightMeasureSpec: Int) {
         val minw: Int = paddingLeft + paddingRight + suggestedMinimumWidth
         val w: Int = resolveSizeAndState(minw, widthMeasureSpec, 1)
         val h: Int = resolveSizeAndState(
-            MeasureSpec.getSize(w),
-            heightMeasureSpec,
-            0
+                MeasureSpec.getSize(w),
+                heightMeasureSpec,
+                0
         )
         widthSize = w
         heightSize = h
         setMeasuredDimension(w, h)
     }
-
 }

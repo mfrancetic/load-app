a9c557bb3b8e4b766ba56bc014fe2bc9dc936f0c
Maja Francetic <francetic.maja@gmail.com>
2020-12-04 15:38:53 +0100

    Fix the MotionLayout transition when entering the DetailActivity

--- starter/app/src/main/java/com/udacity/DetailActivity.kt
+++ starter/app/src/main/java/com/udacity/DetailActivity.kt
@@ -3,7 +3,6 @@ package com.udacity
 import android.graphics.Color
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import androidx.constraintlayout.motion.widget.MotionLayout
 import kotlinx.android.synthetic.main.activity_detail.*
 import kotlinx.android.synthetic.main.content_detail.*
 
@@ -17,47 +16,11 @@ class DetailActivity : AppCompatActivity() {
         cancelAllNotifications(this)
         setFileNameAndStatus()
         setOnClickListener()
-        startTransition()
-//        overridePendingTransition(0, 0)
-    }
-
-    private fun startTransition() {
-        detail_motion_layout.setTransition(R.id.start, R.id.end)
-        detail_motion_layout.setTransitionDuration(3000)
-        detail_motion_layout.transitionToEnd()
-        detail_motion_layout.setTransitionListener(
-            object : MotionLayout.TransitionListener {
-                override fun onTransitionStarted(p0: MotionLayout?, p1: Int, p2: Int) {
-                    println("Transition started")
-                }
-
-                override fun onTransitionChange(p0: MotionLayout?, p1: Int, p2: Int, p3: Float) {
-                    println("Transition changed")
-
-                }
-
-                override fun onTransitionCompleted(p0: MotionLayout?, p1: Int) {
-                    println("Transition completed")
-
-                }
-
-                override fun onTransitionTrigger(
-                    p0: MotionLayout?,
-                    p1: Int,
-                    p2: Boolean,
-                    p3: Float
-                ) {
-                    println("Transition triggered")
-
-                }
-            }
-        )
     }
 
     private fun setOnClickListener() {
         ok_button.setOnClickListener {
-            detail_motion_layout.transitionToStart()
-//            onBackPressed()
+            onBackPressed()
         }
     }
 
